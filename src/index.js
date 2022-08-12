5f3cc238c23d146e3876078384d57a51b8e075fc
Alina <98117067+LinnBlok@users.noreply.github.com>
2022-08-12 23:27:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    let x = n.toString();
+    let strRevers = x.split('').reverse().join('');
+
+
+    if ( n > 0) {
+        return strRevers;
+    }else if (n >= 0) {
+    } else {
+        return strRevers.slice(0, -1);
+    }
